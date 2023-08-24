30e26295cc3a5d88710ed11a2e2b99fe41d4b5c0
mxxxdxxx <127162413+mxxxdxxx@users.noreply.github.com>
2023-08-24 12:11:55 +0900

    Update User.java

--- hackathon07/src/main/java/com/example/hackathon07/domain/User.java
+++ hackathon07/src/main/java/com/example/hackathon07/domain/User.java
@@ -1,32 +1,36 @@
 package com.example.hackathon07.domain;
 
-import jakarta.persistence.Entity;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "Users")
 public class User {
-    @Id
+    @Id @GeneratedValue
+    @Column(name = "User_ID")
     private Long user_id;
 
+    @Column(name = "User_Name")
     private String userName;
+
+    @Column(name = "Password_Hash")
     private String passwordHash;
+
+    @Column(name = "Student_Number")
     private int studentNumber;
+
+    @Column(name = "Level")
     private int level;
 
-    public User(int userID, String userName, String passwordHash, int studentNumber, int level) {
-        this.userID = userID;
+    public User(Long user_id, String userName, String passwordHash, int studentNumber, int level) {
+        this.user_id = user_id;
         this.userName = userName;
         this.passwordHash = passwordHash;
         this.studentNumber = studentNumber;
         this.level = level;
     }
 
-    /**
-     * Getter method 생성
-     */
-
-    public int getUserID() {
-        return userID;
+    public Long getUser_id() {
+        return user_id;
     }
 
     public String getUserName() {
